e755178933aca0cae1f3ab7d61c18ab1e082df43
22051988Di <vitalii.soloninko@gmail.com>
2025-07-22 16:10:55 +0200

    Check service

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getElements() {
+    return [
+      { id: 1, name: 'Hydrogen' },
+      { id: 2, name: 'Helium' },
+    ];
   }
 }
