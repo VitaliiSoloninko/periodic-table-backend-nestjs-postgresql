079772565ec9ac396b9de2115af163bb8879a322
22051988Di <vitalii.soloninko@gmail.com>
2025-07-22 17:34:00 +0200

    Creation of three models: elements, groups, periods

---
+++ src/elements/elements.model.ts
@@ -0,0 +1,70 @@
+import {
+  AllowNull,
+  AutoIncrement,
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+  Unique,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'elements',
+})
+export class Element extends Model<Element> {
+  @PrimaryKey
+  @AutoIncrement
+  @Unique
+  @Column(DataType.INTEGER)
+  id: number;
+
+  @Unique
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  atomic_number: number;
+
+  @Unique
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  symbol: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name: string;
+
+  @Column(DataType.DECIMAL(10, 4))
+  atomic_mass: number;
+
+  @Column(DataType.STRING)
+  state_of_matter: string;
+
+  @Column(DataType.STRING(30))
+  category: string;
+
+  @Column(DataType.DECIMAL(10, 2))
+  boiling_point: number;
+
+  @Column(DataType.DECIMAL(10, 2))
+  melting_point: number;
+
+  @Column(DataType.DECIMAL(4, 2))
+  electronegativity: number;
+
+  @Column(DataType.DECIMAL(10, 4))
+  density: number;
+
+  @Column(DataType.TEXT)
+  discovered_by: string;
+
+  @Column(DataType.STRING)
+  oxidation_states: string;
+
+  // Foreign Keys (nullable for Lanthanides/Actinides)
+  @Column(DataType.INTEGER)
+  group_id: number;
+
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  period_id: number;
+}
---
+++ src/groups/groups.model.ts
@@ -0,0 +1,32 @@
+import {
+  AllowNull,
+  AutoIncrement,
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+  Unique,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'groups',
+})
+export class Group extends Model<Group> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id: number;
+
+  @Unique
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  number: number;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name: string;
+
+  @Column(DataType.STRING)
+  description: string;
+}
