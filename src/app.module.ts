5c54cee3ec4809b95b0c6308d5b3366cc2da0f6d
22051988Di <vitalii.soloninko@gmail.com>
2025-07-22 16:42:27 +0200

    Environment variables. Configuration development.env end production.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5500,
-      username: 'postgres',
-      password: 'admin',
-      database: 'periodic_table',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
