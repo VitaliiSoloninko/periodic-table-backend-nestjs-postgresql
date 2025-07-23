fca9527314ce54c05e6d72c525ffb4f2e331c92a
22051988Di <vitalii.soloninko@gmail.com>
2025-07-23 12:54:25 +0200

    Installation, configuration Swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function start() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Periodensystem-Explorer REST API')
+    .setDescription('Documentation REST API')
+    .setVersion('1.0.0')
+    .addTag('Entwickelt von Vitalii Soloninko')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () =>
     console.log(`Server is running on port ${PORT}`),
